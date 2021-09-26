247106c2b5c0c9f9398668cff84742a8bee01f4a
rdbende <rdbende@gmail.com>
2021-09-26 13:45:53 +0200

    add auto hide

--- tukaan/scrollbar.py
+++ tukaan/scrollbar.py
@@ -13,12 +13,23 @@ class Scrollbar(BaseWidget):
     def __init__(
         self,
         parent: Optional[TkWidget] = None,
+        auto_hide: bool = True,
         focusable: Optional[bool] = None,
         orientation: Optional[Literal["horizontal", "vertical"]] = None,
     ) -> None:
+        self._auto_hide = auto_hide
+
         BaseWidget.__init__(
             self, parent, "ttk::scrollbar", orient=orientation, takefocus=focusable
         )
+
+    @property
+    def auto_hide(self):
+        return self._auto_hide
+
+    @auto_hide.setter
+    def auto_hide(self, should_auto_hide: bool) -> None:
+        self._auto_hide = should_auto_hide
 
     def attach(self, widget: BaseWidget):
         if self.orientation == "vertical":
@@ -38,8 +49,13 @@ class Scrollbar(BaseWidget):
                 )
             self.config(scrollcommand=widget.x_scroll)
 
-    def set(self, first, last) -> None:
-        self._tcl_call(None, self, "set", first, last)
+    def set(self, start: float, end: float) -> None:
+        if self._auto_hide:
+            if float(start) <= 0.0 and float(end) >= 1.0:
+                self.hide()
+            else:
+                self.unhide()
+        self._tcl_call(None, self, "set", start, end)
 
     def get(self) -> None:
         return self._tcl_call((float,), self, "get")
