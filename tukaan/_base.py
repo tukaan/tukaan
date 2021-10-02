ca4e495691b0f4f797250dea64519ea127dfe9ac
rdbende <rdbende@gmail.com>
2021-10-02 22:29:09 +0200

    add the stuff

--- tukaan/_base.py
+++ tukaan/_base.py
@@ -2,14 +2,19 @@ from __future__ import annotations
 
 import collections.abc
 import contextlib
-from functools import partialmethod
+import re
+from functools import partial, partialmethod
 from typing import Any, Callable, Iterator, Literal
 
-from ._constants import _VALID_STATES
+from ._constants import _KEYSYMS, _VALID_BINDINGS, _VALID_STATES
+from ._event import Event
 from ._layouts import LayoutManager
+from ._misc import ScreenDistance, TukaanError
 from ._utils import (
     _callbacks,
     _widgets,
+    create_command,
+    from_tcl,
     get_tcl_interp,
     py_to_tcl_arguments,
     reversed_dict,
@@ -214,6 +219,84 @@ class MethodAndPropMixin:
                 ),
             )
 
+    def _call_bind(
+        self,
+        widget_or_all: MethodAndPropMixin | Literal["all"],
+        sequence: str,
+        func: Callable | Literal[""],
+        overwrite: bool,
+        sendevent: bool,
+        data: Any,
+    ) -> None:
+
+        tcl_sequence = sequence
+        regex_str = r"<Key(Down|Up):(.?)>"
+
+        if sequence in _VALID_BINDINGS:
+            tcl_sequence = _VALID_BINDINGS[sequence]
+        elif re.match(regex_str, sequence):
+            search = re.search(regex_str, sequence)
+            up_or_down = {"Down": "Press", "Up": "Release"}
+            tcl_sequence = (
+                f"<Key{up_or_down[search.group(1)]}-{_KEYSYMS[search.group(2)]}>"
+            )
+
+        def _real_func(func: Callable, data: Any, sequence: str, *args):
+            event = Event()
+            event.data = data
+            event.callback = func
+            event.sequence = sequence
+
+            for (_, type_, attr), string_value in zip(_BINDING_SUBSTS, args):
+                try:
+                    if attr == "keysymbol":
+                        value = reversed_dict(_KEYSYMS)[string_value]
+                    else:
+                        value = from_tcl(type_, string_value)
+                except (ValueError, TukaanError):
+                    # happens when trying to int("??")
+                    value = None
+
+                setattr(event, attr, value)
+            return func() if not sendevent else func(event)
+
+        subst_str = " ".join(subs for subs, *_ in _BINDING_SUBSTS)
+
+        self._tcl_call(
+            None,
+            "bind",
+            widget_or_all,
+            tcl_sequence,
+            f"{'' if overwrite else '+'} if"
+            + f" {{[{create_command(partial(_real_func, func, data, sequence))}"
+            + f" {subst_str}] eq {{break}} }} {{ break }}"
+            if callable(func)
+            else "",
+        )
+
+    def _bind(
+        self,
+        what,
+        sequence: str,
+        func: Callable,
+        overwrite: bool = False,
+        sendevent: bool = False,
+        data=None,
+    ) -> None:
+        self._call_bind(
+            what if what == "all" else self, sequence, func, overwrite, sendevent, data
+        )
+
+    def _unbind(self, what, sequence: str):
+        self._call_bind(
+            what if what == "all" else self, sequence, "", True, False, None
+        )
+
+    bind = partialmethod(_bind, "self")
+    unbind = partialmethod(_unbind, "self")
+    bind_global = partialmethod(_bind, "all")
+    unbind_global = partialmethod(_unbind, "all")
+
 
 class TukaanWidget:
     """Base class for every Tukaan widget"""
@@ -231,6 +314,17 @@ class TkWidget(MethodAndPropMixin):
         self._child_type_count: dict[type, int] = {}
         _widgets[self.tcl_path] = self
         self.child_stats = ChildStatistics(self)
+
+
+_BINDING_SUBSTS = (
+    (r"%W", TkWidget, "widget"),
+    ("%K", str, "keysymbol"),
+    ("%D", float, "delta"),
+    (r"%x", ScreenDistance, "x"),
+    (r"%y", ScreenDistance, "y"),
+    (r"%width", ScreenDistance, "width"),
+    (r"%height", ScreenDistance, "height"),
+)
 
 
 class StateSet(collections.abc.MutableSet):
