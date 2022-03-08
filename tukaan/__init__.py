73b3b9d1aeecfa0c2b3af6800ff194fa550c2515
rdbende <rdbende@gmail.com>
2022-03-08 18:38:27 +0100

    add version checker

--- tukaan/__init__.py
+++ tukaan/__init__.py
@@ -2,6 +2,7 @@ __all__: list = []  # Making wildcard imports impossible. Is it illegal?
 __author__ = "rdbende"
 __package__ = "tukaan"
 __version__ = "0.0.0"
+import sys
 
 from ._events_n_bindings import DragObject, KeySeq
 from ._font import Font
@@ -24,3 +25,28 @@ from .tabview import TabView
 from .textbox import TextBox
 from .timeout import Timer
 from .window import App, Window
+
+
+class _RequiredVersion:
+    def splitver(self, version_str):
+        return tuple(map(int, version_str.split(".")))
+    
+    def __eq__(self, version):
+        if version != __version__:
+            sys.tracebacklimit = 0
+            raise RuntimeError(f"you have Tukaan version {__version__} installed, which is incompatible with your code that requires Tukaan {version}.\n"
+                                f"Use 'pip install tukaan=={version}' to install the required version of Tukaan.")
+
+    def __le__(self, version):
+        if self.splitver(__version__) > self.splitver(version):
+            sys.tracebacklimit = 0
+            raise RuntimeError(f"you have Tukaan version {__version__} installed, which is incompatible with your code that requires Tukaan {version} or older.\n"
+                                f"Use 'pip install tukaan=={version}' to install an older version of Tukaan.")
+
+    def __ge__(self, version):
+        if self.splitver(__version__) < self.splitver(version):
+            sys.tracebacklimit = 0
+            raise RuntimeError(f"you have Tukaan version {__version__} installed, which is incompatible with your code that requires Tukaan {version} or newer.\n"
+                                "Use 'pip install --upgrade tukaan' to install the newest version of Tukaan.")
+
+# required_version = _RequiredVersion()  # uncomment when release to Pypi
