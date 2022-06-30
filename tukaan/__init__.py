93d61ba689f48cf3656124baaf2cbf5628dbe875
Moosems <95927277+Moosems@users.noreply.github.com>
2022-06-30 15:04:49 -0500

    Added to init

--- tukaan/__init__.py
+++ tukaan/__init__.py
@@ -41,6 +41,7 @@ from .widgets.spinbox import SpinBox
 from .widgets.splitview import SplitView
 from .widgets.tabview import TabView
 from .widgets.textbox import TabStop, TextBox
+from .widgets.tooltip import ToolTip
 from .window import App, Window, _ConfigObject
 
 Config = _ConfigObject()
