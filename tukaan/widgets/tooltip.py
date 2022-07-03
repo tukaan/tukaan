9a6ce2bef8afe336aaf48c25c67b7f63971da084
rdbende <rdbende@gmail.com>
2022-07-03 14:50:49 +0200

    Add ToolTipProvider

--- tukaan/widgets/tooltip.py
+++ tukaan/widgets/tooltip.py
@@ -1,45 +1,81 @@
-from tukaan import Label
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from tukaan._tcl import Tcl
+from tukaan.exceptions import TclError
 
-class ToolTip():
-    def __init__(self, window, widget=None, orient="E", *args, **kwargs):
+if TYPE_CHECKING:
+    from tukaan._base import WidgetBase
+
+
+class ToolTipProvider:
+    _widgets: dict[str, str] = {}
+    _schedule_cmd: str = ""
+    _hide_cmd: str = ""
+    _show_cmd: str = ""
+    _setup_done = False
+    _after_id: str = ""
+
+    @classmethod
+    def setup(cls) -> None:
+        cls._schedule_cmd = Tcl.to(cls.schedule)
+        cls._hide_cmd = Tcl.to(cls.hide)
+        cls._show_cmd = Tcl.to(cls.show)
+
         try:
-            self.text = kwargs.pop("text")
-        except:
-            raise Exception("No text provided for ToolTip")
-        self.window = window
-        self.widget = widget
-        self.orient=orient
-        self.widget.bind("<MouseEnter>", self.enter)
-        self.widget.bind("<MouseLeave>", self.leave)
-        self.widget.bind("<MouseDown>", self.leave)
+            Tcl.eval(None, "ttk::style layout ToolTip")
+        except TclError:
+            style = "TButton"
+        else:
+            style = "ToolTip"
 
-    def enter(self, *args):
-        self.tooltip = Label(self.window, text=self.text)
-        Tcl.call(None, self.tooltip, "configure", "-relief", "solid")
-        Tcl.call(None, self.tooltip, "configure", "-borderwidth", "1")
-        X = self.widget.rel_x
-        Y = self.widget.rel_y
-        NY = int(self.widget.rel_y-self.tooltip.height-5)
-        EX = int(self.widget.abs_x+self.widget.width+5)
-        SY = int(self.widget.rel_y+self.widget.height+5)
-        WX = int(self.widget.abs_x-self.tooltip.width-5)
-        if self.orient == "N":
-            self.tooltip.layout.position(x=X, y=NY)
-        elif self.orient == "E":
-            self.tooltip.layout.position(x=EX, y=Y)
-        elif self.orient == "S":
-            self.tooltip.layout.position(x=X, y=SY)
-        elif self.orient == "W":
-            self.tooltip.layout.position(x=WX, y=Y)
-        elif self.orient == "NE":
-            self.tooltip.layout.position(x=EX, y=NY)
-        elif self.orient == "SE":
-            self.tooltip.layout.position(x=EX, y=SY)
-        elif self.orient == "NW":
-            self.tooltip.layout.position(x=WX, y=NY)
-        elif self.orient == "SW":
-            self.tooltip.layout.position(x=WX, y=SY)
-    
-    def leave(self, *args):
-        self.tooltip.destroy()
+        Tcl.call(None, "toplevel", ".tooltip")
+        Tcl.call(None, "wm", "withdraw", ".tooltip")
+        Tcl.eval(None, f"pack [ttk::label .tooltip.label -style {style}] -expand 1 -fill both")
+        Tcl.call(None, "wm", "overrideredirect", ".tooltip", 1)
+
+    @classmethod
+    def add(cls, owner: WidgetBase, message: str) -> None:
+        if not cls._setup_done:
+            cls.setup()
+
+        cls._widgets[owner._lm_path] = message
+
+        Tcl.call(None, "bind", owner._lm_path, "<Enter>", f"+ {cls._schedule_cmd} %W")
+        Tcl.call(None, "bind", owner._lm_path, "<Leave>", f"+ {cls._hide_cmd}")
+        Tcl.call(None, "bind", owner._lm_path, "<ButtonPress>", f"+ {cls._hide_cmd}")
+
+    @classmethod
+    def schedule(cls, widget: str) -> None:
+        message = cls._widgets.get(widget)
+        if message is None:
+            return
+
+        Tcl.call(None, ".tooltip.label", "configure", "-text", message)
+        cls._after_id = Tcl.call(str, "after", 1000, cls._show_cmd, widget)
+
+    @classmethod
+    def hide(cls) -> None:
+        Tcl.call(None, "after", "cancel", cls._show_cmd)
+        Tcl.call(None, "after", "cancel", cls._hide_cmd)
+        Tcl.call(None, "wm", "withdraw", ".tooltip")
+
+    @classmethod
+    def show(cls, widget: str) -> None:
+        owner_x = Tcl.call(int, "winfo", "rootx", widget)
+        owner_width = Tcl.call(int, "winfo", "reqwidth", widget)
+        tip_width = Tcl.call(int, "winfo", "reqwidth", ".tooltip")
+        tip_x = owner_x + owner_width // 2 - tip_width // 2
+
+        owner_y = Tcl.call(int, "winfo", "rooty", widget)
+        tip_height = Tcl.call(int, "winfo", "reqheight", ".tooltip")
+        tip_y = owner_y - tip_height - 5
+
+        if tip_y <= Tcl.call(int, "winfo", "rooty", ".app"):
+            tip_y = owner_y + Tcl.call(int, "winfo", "reqheight", widget) + 5
+
+        Tcl.call(None, "wm", "geometry", ".tooltip", f"+{tip_x}+{tip_y}")
+        Tcl.call(None, "wm", "deiconify", ".tooltip")
+
+        Tcl.call(str, "after", 10000, cls._hide_cmd)
