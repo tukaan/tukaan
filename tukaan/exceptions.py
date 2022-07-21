fc2c442334b79d337938ceadc18fc30f68f51f79
Des <84301435+desvasicek@users.noreply.github.com>
2022-07-21 09:40:27 -0700

    Errors     :D

--- tukaan/exceptions.py
+++ tukaan/exceptions.py
@@ -1,26 +1,49 @@
+# The super().__init__(errormsg) initializes the Exception object and returns an error with custom message (errormsg). Call it like this: raise AppError("Hello, World: There was an error"
+
+# Displayed like this:
+
+# Traceback (most recent call last):
+#   File "/file/path/", line 5, in <module>
+#     raise AppError("Hello, World: There was an error")
+# AppError: Hello, World: There was an error
+
 class TclError(Exception):
-    ...
+    def __init__(self, errormsg):
+        self.errormsg = errormsg
+        super().__init__(errormsg)
 
 
 class AppError(Exception):
-    ...
+    def __init__(self, errormsg):
+        self.errormsg = errormsg
+        super().__init__(errormsg)
 
 
 class WidgetError(Exception):
-    ...
+    def __init__(self, errormsg):
+        self.errormsg = errormsg
+        super().__init__(errormsg)
 
 
 class ColorError(Exception):
-    ...
+    def __init__(self, errormsg):
+        self.errormsg = errormsg
+        super().__init__(errormsg)
 
 
 class FontError(Exception):
-    ...
+    def __init__(self, errormsg):
+        self.errormsg = errormsg
+        super().__init__(errormsg)
 
 
 class LayoutError(Exception):
-    ...
+    def __init__(self, errormsg):
+        self.errormsg = errormsg
+        super().__init__(errormsg)
 
 
 class CellNotFoundError(LayoutError):
-    ...
+    def __init__(self, errormsg):
+        self.errormsg = errormsg
+        super().__init__(errormsg)
