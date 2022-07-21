30a7dcd7bffcada09d6bed0838a4d41e2ee5ea9a
Des <84301435+desvasicek@users.noreply.github.com>
2022-07-21 09:47:30 -0700

    Update exceptions.py

--- tukaan/exceptions.py
+++ tukaan/exceptions.py
@@ -8,42 +8,42 @@
 # AppError: Hello, World: There was an error
 
 class TclError(Exception):
-    def __init__(self, errormsg):
+    def __init__(self, errormsg="There was an error rendering or initializing TCL"):
         self.errormsg = errormsg
         super().__init__(errormsg)
 
 
 class AppError(Exception):
-    def __init__(self, errormsg):
+    def __init__(self, errormsg="There was an error rendering the app"):
         self.errormsg = errormsg
         super().__init__(errormsg)
 
 
 class WidgetError(Exception):
-    def __init__(self, errormsg):
+    def __init__(self, errormsg="There was an error rendering a widget"):
         self.errormsg = errormsg
         super().__init__(errormsg)
 
 
 class ColorError(Exception):
-    def __init__(self, errormsg):
+    def __init__(self, errormsg=="There was an error finding color"):
         self.errormsg = errormsg
         super().__init__(errormsg)
 
 
 class FontError(Exception):
-    def __init__(self, errormsg):
+    def __init__(self, errormsg=="There was an error rendering the font"):
         self.errormsg = errormsg
         super().__init__(errormsg)
 
 
 class LayoutError(Exception):
-    def __init__(self, errormsg):
+    def __init__(self, errormsg=="There was an error rendering getting the layout"):
         self.errormsg = errormsg
         super().__init__(errormsg)
 
 
 class CellNotFoundError(LayoutError):
-    def __init__(self, errormsg):
+    def __init__(self, errormsg="There was an error finding the specified cell"):
         self.errormsg = errormsg
         super().__init__(errormsg)
