acdfe81e894bd255875852866d5563e0d1cdea18
demberto <demberto@proton.me>
2023-01-26 22:24:28 +0530

    add: literal variants for some enums

--- tukaan/enums.py
+++ tukaan/enums.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from enum import Enum
+from typing import Union
+from typing_extensions import Literal
 
 
 class Align(Enum):
@@ -20,15 +22,23 @@ class ImagePosition(Enum):
     Top = "top"
 
     @classmethod
-    def _missing_(cls, value) -> ImagePosition:
+    def _missing_(cls, value: object) -> ImagePosition | None:
         if value == "":
             return cls.Default
+
+
+ImagePositionType = Union[
+    ImagePosition, Literal["bottom", "none", "image", "left", "center", "right", "text", "top"]
+]
 
 
 class Justify(Enum):
     Center = "center"
     Left = "left"
     Right = "right"
+
+
+JustifyType = Union[Justify, Literal["center", "left", "right"]]
 
 
 class Anchor(Enum):
@@ -43,14 +53,23 @@ class Anchor(Enum):
     TopRight = "ne"
 
 
+AnchorType = Union[Anchor, Literal["s", "sw", "se", "center", "w", "e", "n", "nw", "ne"]]
+
+
 class ProgressMode(Enum):
     Determinate = "determinate"
     Indeterminate = "indeterminate"
 
 
+ProgressModeType = Union[ProgressMode, Literal["determinate", "indeterminate"]]
+
+
 class Orientation(Enum):
     Horizontal = "horizontal"
     Vertical = "vertical"
+
+
+OrientationType = Union[Orientation, Literal["horizontal", "vertical"]]
 
 
 class EventQueue(Enum):
@@ -59,6 +78,8 @@ class EventQueue(Enum):
     Immediate = "now"
     AfterPrevious = "mark"
 
+
+EventQueueType = Union[EventQueue, Literal["head", "tail", "now", "mark"]]
 
 # class Location(Enum):
 #    ...
@@ -101,6 +122,11 @@ class WindowState(Enum):
     Normal = "normal"
 
 
+WindowStateType = Union[
+    WindowState, Literal["closed", "fullscreen", "withdrawn", "maximized", "minimized", "normal"]
+]
+
+
 class WindowType(Enum):
     Combo = "combo"
     Desktop = "desktop"
@@ -115,9 +141,32 @@ class WindowType(Enum):
     Splash = "splash"
     ToolBar = "toolbar"
     Tooltip = "tooltip"
+    ToolWindow = "toolwindow"  # xref: tukaan.windows.wm:447
     Utility = "utility"
 
 
+WindowTypeKind = Union[
+    WindowType,
+    Literal[
+        "combo",
+        "desktop",
+        "dialog",
+        "dnd",
+        "dock",
+        "dropdown_menu",
+        "normal",
+        "notification",
+        "popup_menu",
+        "splash",
+        "toolbar",
+        "tooltip",
+        "toolwindow",
+        "utility",
+    ],
+]
+
+
+# ? Make this Windows only
 class WindowBackdropType(Enum):
     Normal = 1
     Mica = 2
