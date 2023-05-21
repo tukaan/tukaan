e78c2c16c745f437d3ef6c98b238727ee1653473
rdbende <rdbende@proton.me>
2023-05-21 17:34:03 +0200

    Because why?

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,14 +6,14 @@ from tukaan._tcl import Tcl
 
 @pytest.fixture(autouse=True, scope="session")
 def app():
-    yield tukaan.App("Test App")
+    return tukaan.App("Test App")
 
 
 @pytest.fixture(scope="session")
 def root():
-    yield tukaan.MainWindow("Test window")
+    return tukaan.MainWindow("Test window")
 
 
 @pytest.fixture()
 def update():
-    yield lambda: Tcl.call(None, "update")
+    return lambda: Tcl.call(None, "update")
